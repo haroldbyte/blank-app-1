0041946ead7cf5ff850bf93626f643a2eaf33512
haroldbyte <ha_mame@hotmail.com>
2025-06-30 17:19:20 +0000

    se mintroducen botones

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 LA NUEVA APLICACION")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "pagina inicial [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("/workspaces/blank-app-1/LOGOCC.png",caption="CONTACTENOS")
+
+left, middle, right = st.columns(3)
+
+
+
+if left.button("BOTON PLANO", use_container_width=False):
+ 
+    left.markdown("HIZO CLICK EN EL BOTON PLANO.")
+if middle.button("Emoji button", icon="😃", use_container_width=True):
+    middle.markdown("USTED HIZO CLICK EN EL BOTON DE EMOJI.")
+if right.button("MATERIALES", icon=":material/mood:", use_container_width=True):
+    right.markdown("CUALES MATERIALES.")
