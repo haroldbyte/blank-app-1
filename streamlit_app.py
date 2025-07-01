906afc066c5bf358c986fbbad104fd941fdb689d
haroldbyte <ha_mame@hotmail.com>
2025-07-01 16:07:00 +0000

    'se adicionaron barras'

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,14 @@ st.write(
     "pagina inicial [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.image("/workspaces/blank-app-1/LOGOCC.png",caption="CONTACTENOS")
+age = st.slider("que valor le calcula hasta 130?", 0, 130, 25)
+
+title = st.text_input("ingrese texto", "escriba aqui")
+st.write("Lo que escribio es.....", title)
+
+number1 = st.number_input("Insert a numero")
+st.write("The current number is ", number1)
+
 
 left, middle, right = st.columns(3)
 
@@ -16,4 +24,4 @@ if left.button("BOTON PLANO", use_container_width=False):
 if middle.button("Emoji button", icon="😃", use_container_width=True):
     middle.markdown("USTED HIZO CLICK EN EL BOTON DE EMOJI.")
 if right.button("MATERIALES", icon=":material/mood:", use_container_width=True):
-    right.markdown("CUALES MATERIALES.")
+    right.markdown("CUALES SON LOS MATERIALES.")
